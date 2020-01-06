1c97bc5cdfd42772a5f15493f77af402324d4f55
Seán Labastille <git@lsr.li>
2020-01-06 12:56:39 +0100

    Add Peter Steinberger to speakers

---
+++ js/speakers.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#speakers',
+    data: {
+        speakers: [
+            {
+                'firstName': 'Peter',
+                'lastName': 'Steinberger',
+                'jobTitle': 'Founder',
+                'company': 'PSPDFKit',
+                'twitter': 'steipete',
+                'picture': 'https://raw.githubusercontent.com/steipete/speaking/master/steipete-avatar-2016%2B.jpg',
+            },
+            {
+                'firstName': 'You',
+                'lastName': 'Could Be Here',
+                'jobTitle': 'Submit',
+                'company': 'Our CfP',
+                'website': 'https://forms.gle/h2U5nU3rK4sUAWZ66',
+                'picture': 'https://appbuilders.ch/img/logo.png',
+            }]
+    },
+});
